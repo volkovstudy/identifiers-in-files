9f49068f4b4ac6fca2dab745547d96518d618784
Volkov Daniil <volkov.study@mail.ru>
2023-01-29 18:17:24 +0300

    Add new method: IdentifierParser#parse(string...)

--- src/indentifier-parser/IdentifierParser.cpp
+++ src/indentifier-parser/IdentifierParser.cpp
@@ -1,1 +1,30 @@
 #include "IdentifierParser.h"
+
+/*
+ * Line view:
+ * <identifier><space><value>
+ */
+
+vector<string> IdentifierParser::split(const string& line, char delimiter) {
+    vector<string> result;
+
+    stringstream stream(line);
+    string item;
+
+    while (getline(stream, item, delimiter)) {
+        result.push_back(item);
+    }
+
+    return result;
+}
+
+IdentifierNode& IdentifierParser::parse(const string& line) {
+    vector<string> values = split(line, ' ');
+
+    string name = values[0];
+    float value = stof(values[1]);
+
+    IdentifierNode* node = new IdentifierNode(name, value);
+
+    return *node;
+}
--- src/indentifier-parser/IdentifierParser.h
+++ src/indentifier-parser/IdentifierParser.h
@@ -2,7 +2,17 @@
 #define IDENTIFIERS_IN_FILES_IDENTIFIERPARSER_H
 
 
+#include "identifier-node/IdentifierNode.h"
+
+#include <string>
+#include <sstream>
+#include <vector>
+
 class IdentifierParser {
+private:
+    static vector<string> split(const string& line, char delimiter);
+public:
+    static IdentifierNode& parse(const string& line);
 };
 
 
